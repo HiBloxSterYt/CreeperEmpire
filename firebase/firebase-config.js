f8ebeb099ec3bbbcbc2193f71d4120eeaa97e1ff
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-04-19 19:36:34 -0400

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,19 @@
+// firebase/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js"; // Or getFirestore if using Firestore
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app); // or getFirestore(app)
+
+export { auth, db };
