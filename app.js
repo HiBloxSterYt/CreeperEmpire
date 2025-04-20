541e614b59b4a60eb756d1c17ec69c19e1e66a05
Max YT <161175370+MaxYT5184@users.noreply.github.com>
2025-04-19 20:13:54 -0400

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import { auth } from './firebase-config.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-
-onAuthStateChanged(auth, user => {
-  const userNameEl = document.getElementById('user-name');
-  if (user) {
-    userNameEl.textContent = `Welcome, ${user.displayName || 'User'}`;
-  }
-});
